4a94a7b952598a57736c2844a904d28efe773393
kevinDlan <kevinkone19@gmail.com>
2021-08-05 12:07:09 +0000

    update

--- CardCreditApp/settings.py
+++ CardCreditApp/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn creditCard.wsgi:application --log-file-
+web: gunicorn CardCreditApp.wsgi
